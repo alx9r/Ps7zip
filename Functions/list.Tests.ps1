6d79f20e970e78ee6f675efbbc4c4b10def17acd
alx9r <eggy.island9@gmail.com>
2015-12-24 19:20:02 -0800

    add list

---
+++ Functions/list.Tests.ps1
@@ -0,0 +1,140 @@
+Import-Module Ps7zip -Force
+
+Describe ConvertFrom-7zListStream {
+    It 'converts.' {
+        $stream = "$($PSCommandPath | Split-Path -Parent)\..\Resources\listSample.xml" |
+            Resolve-Path |
+            Import-Clixml
+
+        $r = $stream | ConvertFrom-7zListStream
+
+        $r -is [pscustomobject] | Should be $true
+
+        $r.VersionNotice | Should be '7-Zip [64] 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18'
+        $r.ArchiveNotice | Should be 'Listing archive: .\vcredist_x64.exe'
+
+        $r.AttributeSections.Count | Should be 3
+        $r.AttributeSections[0].Attributes.Count | Should be 25
+        $r.AttributeSections[0].AttributeLines.Count | Should be 25
+        $r.AttributeSections[1].Attributes.Count | Should be 4
+        $r.AttributeSections[1].AttributeLines.Count | Should be 4
+        $r.AttributeSections[2].Attributes.Count | Should be 5
+        $r.AttributeSections[2].AttributeLines.Count | Should be 5
+        $r.AttributeSections[0].Attributes.CPU | Should be 'x86'
+        $r.AttributeSections[0].Attributes.Type | Should be 'PE'
+
+        $r.Files[0].Name | Should be 'vc_red.cab'
+        $r.Files[0].Size | Should be '1927956'
+        $r.Files.Count | Should be '40'
+    }
+    It 'throws.' {
+        $stream = 'line 1','line 2'
+        {$stream | ConvertFrom-7zListStream} |
+            Should throw 'Error parsing 7zListStream.'
+    }
+}
+Describe Test-7zVersionNoticeLine {
+    It 'false.' {
+        $r = 'Not valid.' | Test-7zVersionNoticeLine
+        $r | Should be $false
+    }
+    It 'true.' {
+        $r = '7-Zip [64] 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18' |
+            Test-7zVersionNoticeLine
+        $r | Should be $true
+    }
+}
+Describe Test-7zArchiveNoticeLine {
+    It 'false.' {
+        $r = 'Not valid.' | Test-7zArchiveNoticeLine
+        $r | Should be $false
+    }
+    It 'true.' {
+        $r = 'Listing archive: setup.exe' |
+            Test-7zArchiveNoticeLine
+        $r | Should be $true
+    }
+}
+Describe Test-7zFileListHeadings {
+    It 'false.' {
+        $r = 'Not valid.' | Test-7zFileListHeadings
+        $r | Should be $false
+    }
+    It 'true.' {
+        $r = '   Date      Time    Attr         Size   Compressed  Name' |
+            Test-7zFileListHeadings
+        $r | Should be $true
+    }
+    It 'does not match empty string.' {
+        $r = [string]::Empty | Test-7zFileListHeadings
+        $r | Should be $false
+    }
+}
+Describe Test-7zAttributeLine {
+    It 'false.' {
+        $r = 'Not valid.' | Test-7zAttributeLine
+        $r | Should be $false
+    }
+    It 'true.' {
+        $r = 'Characteristics = Executable 32-bit NoRelocs NoLineNums NoLocalSyms' |
+            Test-7zAttributeLine
+        $r | Should be $true
+    }
+}
+Describe ConvertFrom-7zAttributeLine {
+    It 'correctly extracts key and value.' {
+        $r = 'Characteristics = Executable 32-bit NoRelocs NoLineNums NoLocalSyms' |
+            ConvertFrom-7zAttributeLine
+
+        $r.Key | Should be 'Characteristics'
+        $r.Value | Should be 'Executable 32-bit NoRelocs NoLineNums NoLocalSyms'
+    }
+}
+Describe Test-7zFileListLine {
+    It 'false.' {
+        $r = 'Not valid.' | Test-7zFileListLine
+        $r | Should be $false
+    }
+    It 'true.' {
+        $r = '2014-05-29 15:19:35 ....A      4249928      1516467  setup.exe' |
+            Test-7zFileListLine
+        $r | Should be $true
+    }
+}
+Describe ConvertFrom-7zFileListLine {
+    It 'correctly extracts key and value.' {
+        $r = '2014-05-29 15:19:35 ....A      4249928      1516467  setup.exe' |
+            ConvertFrom-7zFileListLine
+
+        $r -is [pscustomobject] | Should be $true
+
+        $r.Date | Should be '2014-05-29'
+        $r.Time | Should be '15:19:35'
+        $r.Attr | Should be '....A'
+        $r.Size | Should be '4249928'
+        $r.Compressed | Should be '1516467'
+        $r.Name | Should be 'setup.exe'
+    }
+    It 'correctly handles missing "Compressed" field.' {
+        $r = '2007-11-07 08:44:20 ....A       855040               .\install.exe' |
+            ConvertFrom-7zFileListLine
+
+        $r.Date | Should be '2007-11-07'
+        $r.Time | Should be '08:44:20'
+        $r.Attr | Should be '....A'
+        $r.Size | Should be '855040'
+        $r.Compressed | Should beNullOrEmpty
+        $r.Name | Should be '.\install.exe'
+    }
+}
+Describe Test-7zFileListSummaryLine {
+    It 'false.' {
+        $r = 'Not valid.' | Test-7zFileListSummaryLine
+        $r | Should be $false
+    }
+    It 'true.' {
+        $r = '                              50782596     41805376  36 files, 1 folders' |
+            Test-7zFileListSummaryLine
+        $r | Should be $true
+    }
+}
---
+++ Functions/list.ps1
@@ -0,0 +1,285 @@
+function ConvertFrom-7zListStream
+{
+<#
+.SYNOPSIS
+Converts the output of 7z l to a rich object.
+#>
+    [CmdletBinding()]
+    [OutputType([pscustomobject])]
+    param
+    (
+        # The stream object output by 7z when invoked from PowerShell.
+        [parameter(Mandatory = $true,
+                   Position = 1,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyname=$true)]
+        [object[]]
+        $7zListStream
+    )
+    begin
+    {
+        $accumulator = [System.Collections.ArrayList]@()
+    }
+    process
+    {
+        $7zListStream |
+            % {
+                $accumulator.Add($_) | Out-Null
+            }
+    }
+    end
+    {
+        $h = @{
+            AttributeSections = [System.Collections.ArrayList]@()
+            Files = [System.Collections.ArrayList]@()
+        }
+        $phase = 'find version notice'
+        foreach ( $line in $accumulator )
+        {
+            if
+            (
+                $phase -eq 'find version notice' -and
+                ($line | Test-7zVersionNoticeLine)
+            )
+            {
+                $h.VersionNotice = $line
+                $phase = 'find archive notice'
+                continue
+            }
+
+            if
+            (
+                $phase -eq 'find archive notice' -and
+                ($line | Test-7zArchiveNoticeLine)
+            )
+            {
+                $h.ArchiveNotice = $line
+                $phase = 'extract attributes'
+                continue
+            }
+
+            if
+            (
+                $phase -eq 'extract attributes' -and
+                $line -match '^\-+$'
+            )
+            {
+                $h.AttributeSections.Add(
+                    @{
+                        Attributes = @{}
+                        AttributeLines = [System.Collections.ArrayList]@()
+                    }
+                ) | Out-Null
+                continue
+            }
+
+            if
+            (
+                $phase -eq 'extract attributes' -and
+                ($line | Test-7zAttributeLine)
+            )
+            {
+                $thisSection = $h.AttributeSections[-1]
+                $thisSection.AttributeLines.Add($line) | Out-Null
+                $keyvalue = $line | ConvertFrom-7zAttributeLine
+                $thisSection.Attributes.($keyvalue.Key) = $keyvalue.Value
+                continue
+            }
+
+            if
+            (
+                $phase -eq 'extract attributes' -and
+                ($line | Test-7zFileListHeadings)
+            )
+            {
+                $h.FileListHeading = $line
+                $phase = 'extract file list'
+                continue
+            }
+
+            if
+            (
+                $phase -eq 'extract file list' -and
+                ($line | Test-7zFileListLine)
+            )
+            {
+                $h.Files.Add(($line | ConvertFrom-7zFileListLine)) | Out-Null
+                continue
+            }
+
+            if
+            (
+                $phase -eq 'extract file list' -and
+                ($line | Test-7zFileListSummaryLine)
+            )
+            {
+                $h.FileListSummary = $line
+                $phase = 'complete'
+                continue
+            }
+        }
+
+        if ( $phase -ne 'complete' )
+        {
+            throw New-Object System.ArgumentException(
+                "Error parsing 7zListStream. Ended in phase $phase",
+                '7zListStream'
+            )
+        }
+
+        New-Object psobject -Property $h
+    }
+}
+function Test-7zVersionNoticeLine
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory = $true,
+                   Position = 1,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyname=$true)]
+        [AllowEmptyString()]
+        [string]
+        $Line
+    )
+    process
+    {
+        $Line -match '^7-Zip \[(64|32|86)][0-9\. ]*Copyright \(c\)[0-9,\- ]*Igor Pavlov[0-9\- ]*$'
+    }
+}
+function Test-7zArchiveNoticeLine
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory = $true,
+                   Position = 1,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyname=$true)]
+        [AllowEmptyString()]
+        [string]
+        $Line
+    )
+    process
+    {
+        $Line -match '^Listing archive: .*$'
+    }
+}
+function Test-7zFileListHeadings
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory = $true,
+                   Position = 1,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyname=$true)]
+        [AllowEmptyString()]
+        [string]
+        $Line
+    )
+    process
+    {
+        $Line -match '^ *Date *Time * Attr *Size *Compressed *Name$'
+    }
+}
+
+$attributePattern = '^(?<key>[a-zA-Z0-9 ]*) = (?<value>.*)$'
+function Test-7zAttributeLine
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory = $true,
+                   Position = 1,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyname=$true)]
+        [AllowEmptyString()]
+        [string]
+        $Line
+    )
+    process
+    {
+        $Line -match $attributePattern
+    }
+}
+function ConvertFrom-7zAttributeLine
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory = $true,
+                   Position = 1,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyname=$true)]
+        [string]
+        $Line
+    )
+    process
+    {
+        $groups = ([regex]$attributePattern).Match($line).Groups
+        @{
+            Key = [string]$groups['key']
+            Value = [string]$groups['value']
+        }
+    }
+}
+$fileListPattern = '^(?<Date>[0-9\-]+) +(?<Time>[0-9:]+) (?<Attr>[A-Za-z\.]{5}) +(?<Size>[0-9]+) +((?<Compressed>[0-9]*) +)?(?<Name>.*)$'
+function Test-7zFileListLine
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory = $true,
+                   Position = 1,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyname=$true)]
+        [AllowEmptyString()]
+        [string]
+        $Line
+    )
+    process
+    {
+        $Line -match $fileListPattern
+    }
+}
+function ConvertFrom-7zFileListLine
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory = $true,
+                   Position = 1,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyname=$true)]
+        [string]
+        $Line
+    )
+    process
+    {
+        $groups = ([regex]$fileListPattern).Match($line).Groups
+        $h = @{}
+        'Date','Time','Attr','Size','Compressed','Name' |
+            % { $h.$_ = [string]$groups[$_] }
+        New-Object psobject -Property $h
+    }
+}
+function Test-7zFileListSummaryLine
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory = $true,
+                   Position = 1,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyname=$true)]
+        [AllowEmptyString()]
+        [string]
+        $Line
+    )
+    process
+    {
+        $Line -match '^ +[0-9]+ +[0-9]+ +[0-9]+ files, [0-9]+ folders$'
+    }
+}
